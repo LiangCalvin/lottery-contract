28817da077160befe9acba34aa7dfe8d2ad2259c
Calvin Liang <145249380+LiangCalvin@users.noreply.github.com>
2025-07-06 14:48:26 +0700

    update startLottery, endLottery,  fulfilRandomness

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,69 +1,85 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.6 <0.9.0;
 
-import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
-import { SafeMathChainlink } from "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import {SafeMathChainlink} from "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
-
-contract Lottery {
-
+contract Lottery is VRFConsumerBase, Ownable {
     address payable[] public players;
+    address payable public recentWinner;
     uint256 public usdEntryFee;
-    AggregatorV3Interface internal ethUsdPriceFeed;
+    uint256 public randomness;
 
-    constructor(address _priceFeedAddress) public {
+    AggregatorV3Interface internal ethUsdPriceFeed;
+    enum LOTTERY_STATE {
+        OPEN,
+        CLOSED,
+        CALCULATING_WINNER
+    }
+    LOTTERY_STATE public lottery_state;
+    uint256 public fee;
+    bytes32 public keyHash; // VRF key hash, used to get random number
+    // 0 open, 1 close, 2 cal
+
+    constructor(
+        address _priceFeedAddress,
+        address _vrfCoordinator,
+        address _link,
+        uint256 _fee,
+        bytes32 _keyHash
+    ) public VRFConsumerBase(_vrfCoordinator, _link) {
         // $50 in wei
-        usdEntryFee = 50 * (10 ** 18); 
+        usdEntryFee = 50 * (10 ** 18);
         // Mainnet ETH/USD price feed address
-        ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress); 
+        ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+        lottery_state = LOTTERY_STATE.CLOSED;
+        fee = _fee; // LINK fee for VRF
+        keyHash = _keyHash; // VRF key hash
     }
 
     function enter() public payable {
         // $50 minimum
-        // require();
+        require(lottery_state == LOTTERY_STATE.OPEN);
+        require(msg.value >= getEntranceFee(), "Not enough ETH!");
         players.push(msg.sender);
     }
     function getEntranceFee() public view returns (uint256) {
-        (,int256 price, , ,) = ethUsdPriceFeed.latestRoundData();
-        uint256 adjustPrice = uint256(price) * 10 **10;
-        uint256 costToEnter = (usdEntryFee * 10 **18) / adjustPrice;
+        (, int256 price, , , ) = ethUsdPriceFeed.latestRoundData();
+        uint256 adjustPrice = uint256(price) * 10 ** 10;
+        uint256 costToEnter = (usdEntryFee * 10 ** 18) / adjustPrice;
         return costToEnter;
     }
-    function startLottery() public{
-
+    function startLottery() public onlyOwner {
+        require(
+            lottery_state == LOTTERY_STATE.CLOSED,
+            "can't start new lootery yet"
+        );
+        lottery_state = LOTTERY_STATE.OPEN;
     }
-    function endLottery() public {}
 
+    function endLottery() public {
+        lottery_state = LOTTERY_STATE.CALCULATING_WINNER;
+        bytes32 requestId = requestRandomness(keyHash, fee);
+    }
 
-    // address public manager;
-    // address[] public players;
-
-    // constructor() public {
-    //     manager = msg.sender;
-    // }
-
-    // function enter() public payable {
-    //     require(msg.value > .01 ether, "Minimum amount to enter is 0.01 ether");
-    //     players.push(msg.sender);
-    // }
-
-    // function random() private view returns (uint) {
-    //     return uint(keccak256(abi.encodePacked(block.difficulty, now, players)));
-    // }
-
-    // function pickWinner() public restricted {
-    //     uint index = random() % players.length;
-    //     address winner = players[index];
-    //     payable(winner).transfer(address(this).balance);
-    //     players = new address[](0); // Reset the players array
-    // }
-
-    // modifier restricted() {
-    //     require(msg.sender == manager, "Only the manager can call this function");
-    //     _;
-    // }
-
-    // function getPlayers() public view returns (address[] memory) {
-    //     return players;
-    // }
+    function fulfillRandomness(
+        bytes32 _requestId,
+        uint256 _randomness
+    ) internal override {
+        require(
+            lottery_state == LOTTERY_STATE.CALCULATING_WINNER,
+            "You aren't there yet!"
+        );
+        require(randomness > 0, "random-not-found");
+        uint256 indexOfWinner = randomness % players.length;
+        // address payable recentWinner = players[indexOfWinner];
+        recentWinner = players[indexOfWinner];
+        recentWinner.transfer(address(this).balance);
+        // reset the lottery
+        players = new address payable[](0);
+        lottery_state = LOTTERY_STATE.CLOSED;
+        randomness = _randomness; // reset randomness
+    }
 }
