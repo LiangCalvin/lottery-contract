3244842df29efb3e6bf84d74ea7d281f40d017d0
Calvin Liang <145249380+LiangCalvin@users.noreply.github.com>
2025-07-10 17:06:17 +0700

    new VRFCoordinatorMock

--- contracts/test/VRFCoordinatorMock.sol
+++ contracts/test/VRFCoordinatorMock.sol
@@ -1,99 +1,49 @@
 // SPDX-License-Identifier: MIT
-// An example of a consumer contract that relies on a subscription for funding.
-pragma solidity ^0.8.7;
+pragma solidity 0.6.6;
 
-import "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
-import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
-import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.6/interfaces/LinkTokenInterface.sol";
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
-/**
- * @title The RandomNumberConsumerV2 contract
- * @notice A contract that gets random values from Chainlink VRF V2
- */
-contract VRFConsumerV2 is VRFConsumerBaseV2 {
-    VRFCoordinatorV2Interface immutable COORDINATOR;
-    LinkTokenInterface immutable LINKTOKEN;
+contract VRFCoordinatorMock {
+    LinkTokenInterface public LINK;
 
-    // Your subscription ID.
-    uint64 immutable s_subscriptionId;
+    event RandomnessRequest(
+        address indexed sender,
+        bytes32 indexed keyHash,
+        uint256 indexed seed
+    );
 
-    // The gas lane to use, which specifies the maximum gas price to bump to.
-    // For a list of available gas lanes on each network,
-    // see https://docs.chain.link/docs/vrf-contracts/#configurations
-    bytes32 immutable s_keyHash;
-
-    // Depends on the number of requested values that you want sent to the
-    // fulfillRandomWords() function. Storing each word costs about 20,000 gas,
-    // so 100,000 is a safe default for this example contract. Test and adjust
-    // this limit based on the network that you select, the size of the request,
-    // and the processing of the callback request in the fulfillRandomWords()
-    // function.
-    uint32 immutable s_callbackGasLimit = 100000;
-
-    // The default is 3, but you can set this higher.
-    uint16 immutable s_requestConfirmations = 3;
-
-    // For this example, retrieve 2 random values in one request.
-    // Cannot exceed VRFCoordinatorV2.MAX_NUM_WORDS.
-    uint32 immutable s_numWords = 2;
-
-    uint256[] public s_randomWords;
-    uint256 public s_requestId;
-    address s_owner;
-
-    event ReturnedRandomness(uint256[] randomWords);
-
-    /**
-     * @notice Constructor inherits VRFConsumerBaseV2
-     *
-     * @param subscriptionId - the subscription ID that this contract uses for funding requests
-     * @param vrfCoordinator - coordinator, check https://docs.chain.link/docs/vrf-contracts/#configurations
-     * @param keyHash - the gas lane to use, which specifies the maximum gas price to bump to
-     */
-    constructor(
-        uint64 subscriptionId,
-        address vrfCoordinator,
-        address link,
-        bytes32 keyHash
-    ) VRFConsumerBaseV2(vrfCoordinator) {
-        COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
-        LINKTOKEN = LinkTokenInterface(link);
-        s_keyHash = keyHash;
-        s_owner = msg.sender;
-        s_subscriptionId = subscriptionId;
+    constructor(address linkAddress) public {
+        LINK = LinkTokenInterface(linkAddress);
     }
 
-    /**
-     * @notice Requests randomness
-     * Assumes the subscription is funded sufficiently; "Words" refers to unit of data in Computer Science
-     */
-    function requestRandomWords() external onlyOwner {
-        // Will revert if subscription is not set and funded.
-        s_requestId = COORDINATOR.requestRandomWords(
-            s_keyHash,
-            s_subscriptionId,
-            s_requestConfirmations,
-            s_callbackGasLimit,
-            s_numWords
+    function onTokenTransfer(
+        address sender,
+        uint256 fee,
+        bytes memory _data
+    ) public onlyLINK {
+        (bytes32 keyHash, uint256 seed) = abi.decode(_data, (bytes32, uint256));
+        emit RandomnessRequest(sender, keyHash, seed);
+    }
+
+    function callBackWithRandomness(
+        bytes32 requestId,
+        uint256 randomness,
+        address consumerContract
+    ) public {
+        VRFConsumerBase v;
+        bytes memory resp = abi.encodeWithSelector(
+            v.rawFulfillRandomness.selector,
+            requestId,
+            randomness
         );
+        uint256 b = 206000;
+        require(gasleft() >= b, "not enough gas for consumer");
+        (bool success, ) = consumerContract.call(resp);
     }
 
-    /**
-     * @notice Callback function used by VRF Coordinator
-     *
-     * @param requestId - id of the request
-     * @param randomWords - array of random results from VRF Coordinator
-     */
-    function fulfillRandomWords(
-        uint256 requestId,
-        uint256[] memory randomWords
-    ) internal override {
-        s_randomWords = randomWords;
-        emit ReturnedRandomness(randomWords);
-    }
-
-    modifier onlyOwner() {
-        require(msg.sender == s_owner);
+    modifier onlyLINK() {
+        require(msg.sender == address(LINK), "Must use LINK token");
         _;
     }
 }
